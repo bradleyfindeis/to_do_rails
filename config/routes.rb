3cb2f7b4a47e3ca7b1b58ffe962d748ef291f8bc
Bradley Findeis <bradleyfindeis@gmail.com>
2018-02-26 16:27:14 -0700

    added everything

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+
+  get '/about', to: 'static_pages#about'
+  
+  
+  resources :tasks
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
